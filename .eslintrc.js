f94b659f4922e4ecd3e9f958e0d7177995afcbe9
ato lash <hal-shu-sato@users.noreply.github.com>
2024-02-03 14:17:36 +0900

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "next/core-web-vitals",
+    "prettier",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
