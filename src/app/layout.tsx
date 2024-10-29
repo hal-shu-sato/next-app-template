576803b4867631a16749079bcc45f77ed3b635c3
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2024-10-29 09:31:47 +0900

    recreate next-app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,18 @@
-import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
 
 import type { Metadata } from 'next';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const geistSans = localFont({
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
+});
+const geistMono = localFont({
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,7 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
