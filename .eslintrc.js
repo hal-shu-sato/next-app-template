b0c20a93f1c11daba92153b763fb10fa27fc1a29
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2024-10-29 10:10:57 +0900

    bump typescript-eslint to v8

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: true,
+    projectService: true,
     tsconfigRootDir: __dirname,
   },
   plugins: ['@typescript-eslint'],
